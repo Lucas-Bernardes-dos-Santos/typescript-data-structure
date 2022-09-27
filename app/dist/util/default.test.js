9a68d38d50525c4b46648de526f775212bd40043
Lucas Bernardes dos Santos <lucas.bernardes.santos@proton.me>
2022-09-27 12:30:36 -0300

    Arquivos deletados

--- app/dist/util/default.test.js
+++ app/dist/util/default.test.js
@@ -1,1 +1,9 @@
-"use strict";
+import chalk from "chalk";
+export default class Test {
+    static printLabel() {
+        console.log(`Testing Result: ${chalk.italic('Expected value')} / ${chalk.italic('Amout received')}`);
+    }
+    static printTitleTest(title) {
+        console.log(`${chalk.rgb(235, 179, 16)(`${title}`)}`);
+    }
+}
