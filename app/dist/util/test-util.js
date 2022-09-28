ea9dfa69f9db1ea9b0957c6098189a70dfaf4641
Lucas Bernardes dos Santos <lucas.bernardes.santos@proton.me>
2022-09-28 10:08:25 -0300

    Criado arquivo de funções compartilhadas para serem usadas nos testes das estruturas

---
+++ app/dist/util/test-util.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+const description = chalk.rgb(235, 179, 16); // Laranja
+const extra = chalk.blue;
+export { description };
+export { extra };
---
+++ app/src/util/test-util.ts
@@ -0,0 +1,7 @@
+import chalk from "chalk"
+
+const description = chalk.rgb(235, 179, 16) // Laranja
+const extra = chalk.blue
+
+export {description}
+export {extra}
